069122458c95c252a5caea1acf01bf319a91ccd2
cmkin <191237311@qq.com>
2021-04-28 20:22:02 +0800

    11

--- h5/js/index.js
+++ h5/js/index.js
@@ -8,6 +8,10 @@ axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded
 
 
 
+
+
+
+
 var _this = null
 window.appMain={
 	init:function(){
@@ -20,7 +24,10 @@ window.appMain={
 				activeCai:[],
 				loading:false,
 				tx:false,
-				text:'投票成功'
+				text:'投票成功',
+				dianping_alert:false,
+				dianpingInfos:{},
+				tabActive:0
 			},
 			mounted() {
 				_this = this
@@ -32,6 +39,135 @@ window.appMain={
 				})
 			},
 			methods:{
+				changeTab:function(index){
+					console.log(index)
+					if(index>=2){
+						this.text = "敬请期待!"
+						this.tx = true
+						setTimeout(function(){
+							_this.tx = false
+						},2000)
+						return
+					}
+					this.tabActive = index
+				},
+				showDianping:function(item){
+					this.dianpingInfos = item
+					this.dianping_alert = true
+					setTimeout(function(){
+						_this.showStar()
+					},500)
+					
+					
+					
+				},
+				showStar:function(){
+					var oStar = document.getElementById("star");
+						
+					var aLi = oStar.getElementsByTagName("li");
+						
+					var oUl = oStar.getElementsByTagName("ul")[0];
+						
+					var oSpan = oStar.getElementsByTagName("span")[1];
+						
+					var oP = oStar.getElementsByTagName("p")[0];
+						
+					var i = iScore = iStar = 0;
+						
+					var aMsg = [
+						
+								"很不满意| '' ",
+						
+								"不满意| ''",
+						
+								"一般| ''",
+						
+								"满意| ''",
+						
+								"非常满意| ''"
+						
+								]
+						
+					
+						
+					for (i = 1; i <= aLi.length; i++){
+						
+						aLi[i - 1].index = i;
+						
+						
+						
+						//鼠标移过显示分数
+						
+						aLi[i - 1].onmouseover = function (){
+						
+							fnPoint(this.index);
+						
+							//浮动层显示
+						
+							oP.style.display = "block";
+						
+							//计算浮动层位置
+						
+							oP.style.left = oUl.offsetLeft + this.index * this.offsetWidth - 104 + "px";
+						
+							//匹配浮动层文字内容
+						
+							oP.innerHTML = "<em><b>" + this.index + "</b> 分 " + aMsg[this.index - 1].match(/(.+)\|/)[1] + "</em>" + aMsg[this.index - 1].match(/\|(.+)/)[1]
+						
+						};
+						
+						
+						
+						//鼠标离开后恢复上次评分
+						
+						aLi[i - 1].onmouseout = function (){
+						
+							fnPoint();
+						
+							//关闭浮动层
+						
+							oP.style.display = "none"
+						
+						};
+						
+						
+						
+						//点击后进行评分处理
+						
+						aLi[i - 1].onclick = function (){
+						
+							iStar = this.index;
+						
+							oP.style.display = "none";
+						
+							oSpan.innerHTML = "<strong>" + (this.index) + " 分</strong> "
+						
+						}
+						
+					}
+						
+					
+						
+					//评分处理
+						
+					function fnPoint(iArg){
+						
+						//分数赋值
+						
+						iScore = iArg || iStar;
+						
+						for (i = 0; i < aLi.length; i++) aLi[i].className = i < iScore ? "on" : "";	
+						
+					}
+				},
+				fbpj:function(){
+					this.text = "评价成功"
+					this.tx = true
+					setTimeout(function(){
+						_this.tx = false
+					},2000)
+					this.dianping_alert = false
+				},
 				changeFl:function(id){
 					this.loading = true
 					this.flActive = id
@@ -41,20 +177,21 @@ window.appMain={
 						}
 						_this.cailist = res.data.datas
 						_this.loading = false
+						
 					})
 				},
-				delChange(item){
+				delChange:function(item){
 					this.activeCai.splice(this.activeCai.findIndex(function(tt){
 						return item.category_id  == tt.category_id && item.dishes_id == tt.dishes_id
 					}),1)
 				},
-				getACtiveClass(item){
+				getACtiveClass:function(item){
 					
 					return this.activeCai.some(function(tt){
 					   return item.category_id  == tt.category_id && item.dishes_id == tt.dishes_id
 					}) ? 'active' :'' 
 				},
-				changeCai(item){
+				changeCai:function(item){
 					var ishas = this.activeCai.some(function(tt){
 					   return item.category_id  == tt.category_id && item.dishes_id == tt.dishes_id
 					})
@@ -63,12 +200,24 @@ window.appMain={
 							return item.category_id  == tt.category_id && item.dishes_id == tt.dishes_id
 						}),1)
 					}else{
-						this.activeCai.push(item)
+						var arrs = this.activeCai
+							arrs.push(item)
+						
+						this.activeCai = []
+						this.activeCai = arrs
+
+						this.$forceUpdate()
 					}
 					
 					
+					
+					
+					
+					
+					
+					
 				},
-				submit(){
+				submit:function(){
 					let votes = [
 						
 					]
@@ -120,14 +269,8 @@ window.appMain={
 				}
 			},
 			computed:{
-				getTitles:function(){
-				 var titles = []
-					for(var i in this.cailist){
-						if(_this.cailist[i].active){
-							titles.push(_this.cailist[i])
-						}
-					}
-				  return titles
+				getCactiveCai:function(){
+					return  this.activeCai
 				}
 			}
 		})
